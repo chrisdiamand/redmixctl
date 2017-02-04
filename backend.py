720c195f0711958bcb798c4e94f4c97c4faa8396
Chris Diamand <chris@diamand.org>
2017-02-04 15:59:01 +0000

    Select card and attach mixer

--- backend.py
+++ backend.py
@@ -1,15 +1,54 @@
 #!/usr/bin/env python2.7
 
+
+from __future__ import print_function
+from pyalsa import alsacard, alsamixer
+
+
 class Input:
     def __init__(self, name):
         self.name = name
+
 
 class Output:
     def __init__(self, name):
         self.name = name
 
+
+def find_card_number(cardname):
+    card_numbers = alsacard.card_list()
+    print(card_numbers)
+    ret = None
+    for num in card_numbers:
+        name = alsacard.card_get_name(num)
+        longname = alsacard.card_get_longname(num)
+        print(name, longname)
+
+        if cardname and name == cardname:
+            if not ret:
+                ret = num
+            else:
+                print("Warning: Multiple interfaces matching '%s'" % cardname)
+        elif not cardname:
+            if longname.startswith("Focusrite Scarlett"):
+                if not ret:
+                    ret = num
+                else:
+                    print("Warning: Multiple scarlett interfaces found (at least %d:'%s' and %d:'%s')" % \
+                            (ret, alsacard.card_get_name(ret), num, alsacard.card_get_name(num)))
+
+    return ret
+
+
 class Interface:
-    def __init__(self):
+    def __init__(self, cardname=None):
+        self.card_number = find_card_number(cardname)
+        m = alsamixer.Mixer()
+        print(m)
+        m.attach("hw:%d" % self.card_number)
+        m.load()
+        print(m.list())
+
         self.inputs = [
             Input("Analog 1"),
             Input("Analog 2"),
@@ -25,6 +64,7 @@ class Interface:
             Output("Monitor"),
             Output("Headphone 1"),
             Output("Headphone 2"),
+            Output("SPDIF"),
         ]
 
     def get_inputs(self):
