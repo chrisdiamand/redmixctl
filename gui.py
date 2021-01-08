885689f9c1ce7cfc7c20eeaa7eb9ecb43e3c2ed5
Chris Diamand <chris@diamand.org>
2021-01-08 22:14:30 +0000

    Add output settings panel

--- gui.py
+++ gui.py
@@ -35,6 +35,8 @@ class Fader(wx.Window):
 
         sizer = wx.GridBagSizer()
         slider = wx.Slider(self, wx.ID_ANY, style=wx.SL_VERTICAL | wx.SL_INVERSE)
+        self.Bind(wx.EVT_SLIDER, self.update, slider)
+
         sizer.Add(slider, (1, 1), span=(10, 3), flag=wx.EXPAND)
 
         label = wx.StaticText(self, wx.ID_ANY, label=source.name)
@@ -42,6 +44,9 @@ class Fader(wx.Window):
 
         self.SetSizerAndFit(sizer)
         self.Hide()
+
+    def update(self, event):
+        logger.info("%s changed to %d", self.source.name, event.GetInt())
 
 
 class MixerTab(wx.Window):
@@ -91,15 +96,13 @@ class MixerTabs(wx.Notebook):
             self.AddPage(mix_tab, mix_name)
 
 
-class CommonSettingsPanel(wx.Panel):
+class InputSettingsPanel(wx.Panel):
     def __init__(self, parent, app, iface):
         wx.Panel.__init__(self, parent)
         self.app = app
         self.iface = iface
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-
-        sizer.Add(wx.StaticText(self, label="Inputs to monitor"))
+        sizer = wx.StaticBoxSizer(wx.VERTICAL, self, "Inputs to monitor")
 
         choices = []
         enabled_sources = []
@@ -112,7 +115,7 @@ class CommonSettingsPanel(wx.Panel):
         self.monitorable_inputs.SetCheckedStrings(enabled_sources)
         self.Bind(wx.EVT_CHECKLISTBOX, self.monitorable_inputs_changed, self.monitorable_inputs)
 
-        sizer.Add(self.monitorable_inputs)
+        sizer.Add(self.monitorable_inputs, flag=wx.ALL | wx.EXPAND, border=5)
         self.SetSizerAndFit(sizer)
 
         self.Show()
@@ -137,16 +140,42 @@ class CommonSettingsPanel(wx.Panel):
             mix_tab.update()
 
 
+class OutputSettingsPanel(wx.Panel):
+    def __init__(self, parent, app, iface):
+        wx.Panel.__init__(self, parent)
+        self.app = app
+        self.iface = iface
+
+        panel_sizer = wx.StaticBoxSizer(wx.VERTICAL, self, "Outputs")
+        #panel_sizer.Add(wx.StaticText(self, label="Outputs"))
+
+        outputs_sizer = wx.GridSizer(4)
+
+        self.outputs = []
+        for output in self.iface.get_outputs():
+            outputs_sizer.Add(wx.StaticText(self, wx.ID_ANY, label=output.name), 50,
+            wx.ALIGN_CENTRE_VERTICAL | wx.ALIGN_RIGHT)
+            mix_selector = wx.Choice(self, choices=self.iface.model.mixes + ["Off"])
+            outputs_sizer.Add(mix_selector, 0, wx.ALIGN_CENTRE)
+
+        panel_sizer.Add(outputs_sizer, flag=wx.ALL, border=5)
+
+        self.SetSizerAndFit(panel_sizer)
+        self.Show()
+
+
 class MainWindow(wx.Frame):
     def __init__(self, app, iface):
         wx.Frame.__init__(self, None, wx.ID_ANY, "Scarlett Mixer")
 
-        self.settings = CommonSettingsPanel(self, app, iface)
+        self.input_settings = InputSettingsPanel(self, app, iface)
         self.tabs = MixerTabs(self, iface)
+        self.output_settings = OutputSettingsPanel(self, app, iface)
 
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.settings)
-        sizer.Add(self.tabs)
+        sizer.Add(self.input_settings, proportion=0, flag=wx.ALL, border=5)
+        sizer.Add(self.tabs, proportion=1, flag=wx.ALL, border=5)
+        sizer.Add(self.output_settings, proportion=0, flag=wx.ALL, border=5)
         self.SetSizerAndFit(sizer)
 
         self.Show(True)
