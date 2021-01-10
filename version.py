e84a67cee956dca64c32f9a6e97c03c5178966f5
Chris Diamand <chris@diamand.org>
2021-01-10 12:23:13 +0000

    Update copyrights and shebangs

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017 Chris Diamand
+# Copyright 2017, 2021 Chris Diamand
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
